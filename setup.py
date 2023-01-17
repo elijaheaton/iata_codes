6650f5a5b89851a1b81b8a4a0978323ee085eaf0
Elijah Eaton <eaton.elijah@gmail.com>
2023-01-16 19:16:39 -0600

    creates csv file in v0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name='iata_codes',
-        version='0.2',
+        version='0.3',
         description='A database for iata codes',
         author='Elijah Eaton',
         author_email='eaton.elijah@gmail.com',
