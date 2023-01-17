c3dd388d2a51e9851d595ba2dc8d6364fa621e4a
Elijah Eaton <eaton.elijah@gmail.com>
2023-01-16 18:56:34 -0600

    added to setup

--- setup.py
+++ setup.py
@@ -3,4 +3,15 @@
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(
+        name='iata_codes',
+        version='0.1',
+        description='A database for iata codes',
+        author='Elijah Eaton',
+        author_email='eaton.elijah@gmail.com',
+        install_requires=[
+            'pandas',
+            'beauitfulsoup4',
+            'lxml',
+        ],
+    )
