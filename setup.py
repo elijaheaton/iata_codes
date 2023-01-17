bf75221bf813918bf643aa5e6509168d9b916711
Elijah Eaton <eaton.elijah@gmail.com>
2023-01-16 19:07:19 -0600

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name='iata_codes',
-        version='0.1',
+        version='0.2',
         description='A database for iata codes',
         author='Elijah Eaton',
         author_email='eaton.elijah@gmail.com',
